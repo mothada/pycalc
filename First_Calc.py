654a330cbe545aa6dab3c5f5dbb3c1e614c09f2a
mothada <44650635+mothada@users.noreply.github.com>
2018-12-05 19:02:35 +0000

    Add files via upload

---
+++ First_Calc.py
@@ -0,0 +1,20 @@
+#print "Hallow World"
+
+def func_x():
+    print "hallow world"
+    func_y(343)
+
+def func_y(x):
+    print x
+
+def test_calc():
+    assert func_x(3) == 1
+"""""
+if __name__=="__main__":
+    func_x()
+
+
+
+if __name__=="__main__":
+    func_y(x)
+"""
